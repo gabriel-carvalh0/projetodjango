c5d3c794ed31d9eb9117272036823797d3844f04
Gabriel <gabrielmoraiscarvalho659@gmail.com>
2021-12-09 11:46:05 -0300

    exibindo erros no formulario

--- app_ecommerce/views.py
+++ app_ecommerce/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import RequestAborted
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,6 +6,7 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from app_ecommerce.forms import ContactForm
 
 from catalog.models import Category
 
@@ -17,7 +19,14 @@ def index(request):
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+    else:
+        form = ContactForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'contact.html', context)
 
 
     
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # libs
+    'widget_tweaks',
+    # apps
     'app_ecommerce',
     'catalog',  
 ]
