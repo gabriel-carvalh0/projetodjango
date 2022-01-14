cfe6da7a666ddf57a8149b46dbcc7053bb00d895
Gabriel <gabrielmoraiscarvalho659@gmail.com>
2022-01-14 14:51:05 -0300

    login com E-mail

---
+++ accounts/backends.py
@@ -0,0 +1,18 @@
+import email
+import imp
+from django.contrib.auth.backends import ModelBackend as BaseModelBackend
+from django.contrib.auth import get_user_model
+
+from .models import User
+
+class ModelBackend(BaseModelBackend):
+
+    def authenticate(self, username=None, password=None):
+        if username is None:
+            try:
+                user = User.objects.get(email=username)
+                if user.check_password(password):
+                    return user
+            except User.DoesNotExist:
+                pass
+
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -150,6 +150,10 @@ LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.ModelBackend',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
